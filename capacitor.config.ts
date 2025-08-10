0e476535ac7e6f772080f181b290323a3075f6dc
Marjon Godito <marjon.godito@gmanmi.com>
2025-08-10 18:23:08 +0800

    google authentication fix - part 2

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.putahe.app',
   appName: 'Putahe',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    FirebaseAuthentication: {
+      authDomain: undefined,
+      skipNativeAuth: false,
+      providers: ["google.com"]
+    }
+  }
 };
 
 export default config;
